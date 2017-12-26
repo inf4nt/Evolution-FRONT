dd956bbf6e9fbdcc48cc871bedb27cd545fa4398
infant <com.infant@gmail.com>
2017-12-27 01:13:14 +0200

    fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const path = require('path');
 app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
