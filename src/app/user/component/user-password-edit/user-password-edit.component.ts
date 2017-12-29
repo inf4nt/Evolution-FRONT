2d47dcd16582b323f0ff729edfa3b25dbbc8e68f
infant <com.infant@gmail.com>
2017-12-29 19:53:12 +0200

    Fixed package.json

---
+++ src/app/user/component/user-password-edit/user-password-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-password-edit',
+  templateUrl: './user-password-edit.component.html',
+  styleUrls: ['./user-password-edit.component.css']
+})
+export class UserPasswordEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,16 +3,15 @@ import {UserCreateComponent} from "./component/user-create/user-create.component
 import {UserEditComponent} from "./component/user-edit/user-edit.component";
 import {UserListComponent} from "./component/user-list/user-list.component";
 import {RouterModule, Routes} from "@angular/router";
-import {IsAuthGuard} from "../guard/is-auth-guard";
 import {SharedModule} from "../shared/shared.module";
 import {UserHomeComponent} from "./component/user-home/user-home.component";
 import {UserSettingsComponent} from "./component/user-settings/user-settings.component";
-import {FeedPostComponent} from "../feed/component/feed-post/feed-post.component";
-import {FeedListComponent} from "../feed/component/feed-list/feed-list.component";
 import {FriendModule} from "../friend/friend.module";
 import {FeedModule} from "../feed/feed.module";
 import {RegistrationComponent} from "./component/user-registration/registration.component";
 import {NgxPaginationModule} from "ngx-pagination";
+import {UserPasswordEditComponent} from "./component/user-password-edit/user-password-edit.component";
+import {UserProfileComponent} from "./component/user-profile/user-profile.component";
 
 const appRoutes: Routes = [
   {path: 'user-list', component: UserListComponent},
@@ -42,7 +41,9 @@ const appRoutes: Routes = [
     RegistrationComponent,
     UserListComponent,
     UserSettingsComponent,
-    UserHomeComponent
+    UserHomeComponent,
+    UserPasswordEditComponent,
+    UserProfileComponent,
   ]
 })
 export class UserModule {
