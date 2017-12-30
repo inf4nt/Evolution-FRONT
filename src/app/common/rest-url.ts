80ce4b6cd5c9b9e7840ef9380eff4a29a33f8927
infant <com.infant@gmail.com>
2017-12-30 20:05:21 +0200

    change rest url

--- src/app/common/rest-url.ts
+++ src/app/common/rest-url.ts
@@ -1,6 +1,6 @@
-export let serverUrl = 'http://localhost:8585/';
+// export let serverUrl = 'http://localhost:8585/';
 
-// export let serverUrl = 'https://evolution-social-rest-server.herokuapp.com/';
+export let serverUrl = 'https://evolution-social-rest-server.herokuapp.com/';
 
 export let findOneUser = serverUrl + 'user/';
 
